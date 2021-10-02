ef0d7e94388e40e44eb2729d61ffa53e9898b0d0
GjorgjievaS <gorgieva.sg@gmail.com>
2021-10-02 14:02:55 +0200

    first

---
+++ DailyTemperatureTest.java
@@ -0,0 +1,109 @@
+package DailyTemperatures;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.*;
+
+
+class Day{
+    private int day;
+    private List<Double> temperatures;
+    private char scale;
+
+    public Day(int day, List<Double> temperatures, char scale) {
+        this.day = day;
+        this.temperatures = temperatures;
+        this.scale = scale;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public List<Double> getTemperatures() {
+        return temperatures;
+    }
+
+    public char getScale() {
+        return scale;
+    }
+
+    public double getMinTemp(){
+        return temperatures.stream().min(Double::compareTo).get();
+    }
+
+    public double getMaxTemp(){
+        return temperatures.stream().max(Double::compareTo).get();
+    }
+
+    public double getAvgTemp(){
+        return temperatures.stream().mapToDouble(t->t).sum()/(temperatures.size()*1.0);
+    }
+
+    public void convertToC(){
+        for (int i=0;i<temperatures.size();i++){
+            temperatures.set(i, ((temperatures.get(i)-32)*5)/9);
+        }
+        this.scale = 'C';
+    }
+
+    public void convertToF(){
+        for (int i=0;i<temperatures.size();i++){
+            temperatures.set(i, ((temperatures.get(i)*9)/5)+32);
+        }
+        this.scale = 'F';
+    }
+}
+
+class DailyTemperatures{
+    Set<Day> days;
+
+    public DailyTemperatures(){
+        this.days = new TreeSet<>(Comparator.comparing(Day::getDay));
+    }
+
+    public void readTemperatures(InputStream inputStream){
+        Scanner sc = new Scanner(inputStream);
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] parts = line.split("\\s+");
+
+            Integer day = Integer.parseInt(parts[0]);
+            char scale = parts[1].charAt(parts[1].length()-1);
+            List<Double> temps = new ArrayList<>();
+
+            for (int i=1;i<parts.length;i++){
+                temps.add(Double.parseDouble(parts[i].substring(0,parts[i].length()-1)));
+            }
+
+            days.add(new Day(day,temps,scale));
+        }
+    }
+
+    public void writeDailyStats(OutputStream outputStream, char scale) {
+
+        days.stream().forEach(day -> {
+            if (day.getScale() != scale) {
+                if (scale == 'F')
+                    day.convertToF();
+                else
+                    day.convertToC();
+            }
+
+            System.out.println(String.format("%3d: Count: %3d Min: %6.2f%c Max: %6.2f%c Avg: %6.2f%c",
+                    day.getDay(), day.getTemperatures().size(), day.getMinTemp(), scale, day.getMaxTemp(), scale, day.getAvgTemp(), scale));
+        });
+    }
+}
+
+public class DailyTemperatureTest {
+    public static void main(String[] args) {
+        DailyTemperatures dailyTemperatures = new DailyTemperatures();
+        dailyTemperatures.readTemperatures(System.in);
+        System.out.println("=== Daily temperatures in Celsius (C) ===");
+        dailyTemperatures.writeDailyStats(System.out, 'C');
+        System.out.println("=== Daily temperatures in Fahrenheit (F) ===");
+        dailyTemperatures.writeDailyStats(System.out, 'F');
+    }
+}
---
+++ Shapes2Test.java
@@ -0,0 +1,171 @@
+package Canvas_2;
+
+import java.io.*;
+import java.util.*;
+
+class IrregularCanvasException extends Exception{
+    public IrregularCanvasException(String id,double maxArea){
+        super(String.format("Canvas %s has a shape with area larger than %.2f",id,maxArea));
+    }
+}
+enum TYPE {
+    CIRCLE,
+    SQUARE
+}
+abstract class Shape{
+    int size;
+
+    public Shape(int size){
+        this.size = size;
+    }
+
+    abstract double area();
+
+    abstract TYPE getType();
+
+    public static Shape createShape(int size, char type, double maxArea){
+        switch (type){
+            case 'S':
+                return new Square(size);
+            case 'C':
+                return new Circle(size);
+            default:
+                return null;
+        }
+    }
+}
+
+class Circle extends Shape{
+
+    public Circle(int size) {
+        super(size);
+    }
+
+    @Override
+    double area() {
+        return size*size*Math.PI;
+    }
+
+    @Override
+    TYPE getType() {
+        return TYPE.CIRCLE;
+    }
+}
+
+class Square extends Shape{
+
+    public Square(int size) {
+        super(size);
+    }
+
+    @Override
+    double area() {
+        return size*size;
+    }
+
+    @Override
+    TYPE getType() {
+        return TYPE.SQUARE;
+    }
+}
+
+class Canvas implements Comparable<Canvas> {
+    String id;
+    List<Shape> shapes;
+
+    public Canvas(String id, List<Shape> shapes) {
+        this.id = id;
+        this.shapes = shapes;
+    }
+
+
+    @Override
+    public int compareTo(Canvas o) {
+        return Double.compare(
+                this.shapes.stream().mapToDouble(m->m.area()).sum(),
+                o.shapes.stream().mapToDouble(m->m.area()).sum()
+        );
+    }
+
+    public int circlesCount(){
+        return (int) shapes.stream().filter(s->s.getType().equals(TYPE.CIRCLE)).count();
+    }
+
+    @Override
+    public String toString() {
+        //ID total_shapes total_circles total_squares min_area max_area average_area
+        DoubleSummaryStatistics dss = shapes.stream().mapToDouble(i->i.area()).summaryStatistics();
+        return String.format("%s %d %d %d %.2f %.2f %.2f",
+                id,
+                shapes.size(),
+                circlesCount(),
+                shapes.size()-circlesCount(),
+                dss.getMin(),
+                dss.getMax(),
+                dss.getAverage());
+    }
+
+    public static Canvas createCanvas(String line, double maxArea) throws IrregularCanvasException {
+
+        String[] parts = line.split("\\s+");
+        String id = parts[0];
+        List<Shape> shapes = new ArrayList<>();
+
+        for (int i=1;i<parts.length;i+=2){
+            Shape s = Shape.createShape(Integer.parseInt(parts[i+1]), parts[i].charAt(0),maxArea);
+            if (s.area() > maxArea)
+                throw new IrregularCanvasException(id,maxArea);
+            shapes.add(Shape.createShape(Integer.parseInt(parts[i+1]),parts[i].charAt(0),maxArea));
+        }
+        return new Canvas(id,shapes);
+    }
+}
+
+class ShapesApplication {
+    List<Canvas> canvas;
+    double maxArea;
+
+    public ShapesApplication(double maxArea) {
+        this.maxArea = maxArea;
+        this.canvas = new ArrayList<>();
+    }
+
+    public void readCanvases (InputStream inputStream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+
+        while ((line=br.readLine())!=null) {
+
+            try {
+                canvas.add(Canvas.createCanvas(line,maxArea));
+            }catch (IrregularCanvasException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void printCanvases (OutputStream os){
+        PrintWriter pw = new PrintWriter(os);
+
+        canvas.stream().sorted(Comparator.reverseOrder()).forEach(pw::println);
+        pw.flush();
+        pw.close();
+    }
+}
+
+
+public class Shapes2Test {
+
+    public static void main(String[] args) throws IOException {
+
+        ShapesApplication shapesApplication = new ShapesApplication(10000);
+
+        System.out.println("===READING CANVASES AND SHAPES FROM INPUT STREAM===");
+        shapesApplication.readCanvases(System.in);
+
+        System.out.println("===PRINTING SORTED CANVASES TO OUTPUT STREAM===");
+        shapesApplication.printCanvases(System.out);
+
+
+    }
+}
